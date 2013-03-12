3b0594db3c7f0712318fb283959fd7affe3f2de2
Devaraj <nsdevaraj@gmail.com>
2013-03-12 08:00:09 +0530

    login fixed

---
+++ Tango/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  Tango
+//
+//  Created by Devaraj NS on 12/03/13.
+//  Copyright (c) 2013 Devaraj NS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIView
+
+@end
---
+++ Tango/LoginView.m
@@ -0,0 +1,31 @@
+//
+//  LoginView.m
+//  Tango
+//
+//  Created by Devaraj NS on 12/03/13.
+//  Copyright (c) 2013 Devaraj NS. All rights reserved.
+//
+
+#import "LoginView.h"
+
+@implementation LoginView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
